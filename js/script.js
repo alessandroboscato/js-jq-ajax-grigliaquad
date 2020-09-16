2d5ccf74de3df4b72e6396b68df8aa83f3088f29
Alessandro <alessandro.boscato@gmail.com>
2020-09-16 20:50:20 +0200

    genera il box con handlebars (manca if)

--- js/script.js
+++ js/script.js
@@ -3,41 +3,37 @@ $(document).ready(function() {
   // Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
   // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
   // Il numero ottenuto appare al centro del quadrato.
+var source = $("#entry-template").html();
+var template = Handlebars.compile(source);
+
  $("#generate").click(
    function() {
-     $(".box").each(
-       function () {
-         var box = $(this);
-         $.ajax(
-       {
-       "url": "https://flynn.boolean.careers/exercises/api/random/int",
-       "method": "GET",
-       "success": function (data, stato) {
-       var rispostaServer = data.response;
-        box.find("span").text(rispostaServer);
-        if (rispostaServer < 5) {
-          box.removeClass("bg_green");
-          box.addClass("bg_yellow");
-        } else {
-          box.removeClass("bg_yellow");
-          box.addClass("bg_green");
-        }
+     $.ajax(
+      {
+      "url": "https://flynn.boolean.careers/exercises/api/random/int",
+      "method": "GET",
+      "success": function (data, stato) {
+      var responseServer = data.response;
+      var context = {
+        "number": responseServer
+      };
+      var html = template(context);
+      $("#box-wrapper").append(html);
+      },
+      "error": function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+      }
+      });
+console.log($.ajax());
+//click generate ajax
+    });
 
-       },
-       "error": function (richiesta, stato, errori) {
-       alert("E' avvenuto un errore. " + errore);
-       }
-       });
-
-       }
-     )
-
+//ready
    });
 
-});
 
-    // var source = document.getElementById("entry-template").innerHTML;
-    // var template = Handlebars.compile(source);
+    //
+    //
     // var context = {
     //   "success": rispostaServer,
     // };
