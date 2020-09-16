15b6b25025bbc2955d8ceeb5321c011b8ee4084f
Alessandro <alessandro.boscato@gmail.com>
2020-09-16 17:45:40 +0200

    griglia 6x6 e stampo numero (errore)

--- js/script.js
+++ js/script.js
@@ -3,34 +3,33 @@ $(document).ready(function() {
   // Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
   // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
   // Il numero ottenuto appare al centro del quadrato.
+ $("#generate").click(
+   function() {
+     $.ajax(
+   {
+   "url": "https://flynn.boolean.careers/exercises/api/random/int",
+   "method": "GET",
+   "success": function (data, stato) {
+   var rispostaServer = data.response;
+   $(".box span").each(
+     function () {
+       $(this).text(rispostaServer);
+     }
+   )
 
-
-
-
-  $("#generate").click(
-    function() {
-      $.ajax(
-    {
-    "url": "https://flynn.boolean.careers/exercises/api/random/int",
-    "method": "GET",
-    "success": function (data, stato) {
-    var rispostaServer = data.response;
-    var source = document.getElementById("entry-template").innerHTML;
-    var template = Handlebars.compile(source);
-    var context = {
-      "number": rispostaServer,
-    };
-    var html = template(context);
-    $("#box-wrapper").append(html);
-    },
-    "error": function (richiesta, stato, errori) {
-    alert("E' avvenuto un errore. " + errore);
-    }
-    });
-
-
-
-    });
-
+   },
+   "error": function (richiesta, stato, errori) {
+   alert("E' avvenuto un errore. " + errore);
+   }
+   });
+   });
 
 });
+
+    // var source = document.getElementById("entry-template").innerHTML;
+    // var template = Handlebars.compile(source);
+    // var context = {
+    //   "success": rispostaServer,
+    // };
+    // var html = template(context);
+    // $("#box-wrapper").append(html);
