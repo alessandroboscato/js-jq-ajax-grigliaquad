fb4c9cbb4920a4ce6dcba0dccb6fcab58dc79948
Alessandro <alessandro.boscato@gmail.com>
2020-09-16 16:30:12 +0200

    button click -> chiamata ajax

--- js/script.js
+++ js/script.js
@@ -3,16 +3,24 @@ $(document).ready(function() {
   // Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
   // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
   // Il numero ottenuto appare al centro del quadrato.
-    $.ajax(
-  {
-  "url": "https://flynn.boolean.careers/exercises/api/random/int",
-  "method": "GET",
-  "success": function (data, stato) {
-  $("#risultati").html(data);
-  },
-  "error": function (richiesta, stato, errori) {
-  alert("E' avvenuto un errore. " + errore);
-  }
-  });
+
+  $("#generate").click(
+    function() {
+      $.ajax(
+    {
+    "url": "https://flynn.boolean.careers/exercises/api/random/int",
+    "method": "GET",
+    "success": function (data, stato) {
+    var rispostaServer = data.response;
+    console.log(rispostaServer);
+
+    },
+    "error": function (richiesta, stato, errori) {
+    alert("E' avvenuto un errore. " + errore);
+    }
+    });
+
+    });
+
 
 });
